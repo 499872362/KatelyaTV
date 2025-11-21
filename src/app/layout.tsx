e79c9b30d2c35766224bc99b83f7a89eea62fe2a
大毛的春天 <140038284+499872362@users.noreply.github.com>
2025-11-21 19:47:14 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default async function RootLayout({
         />
       </head>
       <body
-        className={`${inter.classine} min-h-screen bg-white text-gray-900 dark:bg-black dark:text-gray-200`}
+        className={`${inter.className} min-h-screen bg-white text-gray-900 dark:bg-black dark:text-gray-200`}
       >
         {/* 浮动几何形状装饰 */}
         <FloatingShapes />
